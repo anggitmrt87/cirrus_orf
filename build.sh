581d9003362b2505de549cdcaeadef4f962228e7
anggitmrt87 <jarbull87@gmail.com>
2025-10-21 12:43:35 +0700

    anu⁴

---
+++ build.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash
+
+set -e
+export TERM=xterm-256color
+
+# Source config functions
+source .cirrus_config.sh
+
+cd OrangeFox/fox_${FOX_SYNC_BRANCH}
+
+echo -e "\033[0;36m##########################################\033[0m"
+echo -e "\033[0;36m$(figlet "OrangeFox")\033[0m"
+echo -e "\033[0;36m##########################################\033[0m"
+
+source build/envsetup.sh
+export ALLOW_MISSING_DEPENDENCIES=true
+
+BUILDLOG="build.log"
+DEVICE=$(grep "PRODUCT_MODEL :=" ${DEVICE_PATH}/twrp_*.mk -m 1 | cut -d = -f 2)
+
+build_message "lunch twrp_${DEVICE_NAME}-eng"
+lunch twrp_${DEVICE_NAME}-eng
+sleep 5
+
+build_message "Building... 🛠️"
+mkfifo -m 644 reading
+tee -a ${BUILDLOG} < reading & progress & mka adbd ${BUILD_TARGET}image -j$(nproc --all) > reading
+retVal=$?
+
+timeEnd
+statusBuild
+
+# Extract build information
+EV1=$(TZ=Asia/Jakarta date +%Y%m%d)
+EV2=$(grep "PRODUCT_MODEL :=" ${DEVICE_PATH}/twrp_*.mk -m 1 | cut -d = -f 2)
+EV3=$(ls -lh out/target/product/${DEVICE_NAME}/OrangeFox*.zip | cut -d ' ' -f5)
+EV4=$(md5sum out/target/product/${DEVICE_NAME}/OrangeFox*.zip | cut -d ' ' -f1)
+EV5=$(sha1sum out/target/product/${DEVICE_NAME}/OrangeFox*.zip | cut -d ' ' -f1)
+EV6=$(cd ${DEVICE_PATH} && git log --pretty=format:'%s' -1)
+EV10=$(grep "#### build completed successfully" ${BUILDLOG} -m 1 | cut -d '(' -f 2)
+
+export BUILD_DATE=${EV1}
+export DEVICE=${EV2}
+export ORF_SIZE=${EV3}
+export ORF_MD5=${EV4}
+export ORF_SHA1=${EV5}
+export DT_COMMIT=${EV6}
+export ORF_ACTOR=${CIRRUS_REPO_OWNER}
+export ORF_REPONAME=${CIRRUS_REPO_NAME}
+export ORF_ID=${CIRRUS_BUILD_ID}
+export ORF_TIME=${EV10}
+
+if [[ "${GH_RELEASE}" == "true" ]] && [[ -n "$GITHUB_TOKEN" ]]; then
+    source .cirrus_config.sh
+    bash ./.cirrus_notes.sh
+    
+    gh release create ${CIRRUS_BUILD_ID} \
+        out/target/product/${DEVICE_NAME}/OrangeFox*.img \
+        out/target/product/${DEVICE_NAME}/OrangeFox*.zip \
+        out/target/product/${DEVICE_NAME}/OrangeFox*.zip.md5 \
+        --title "🦊 OrangeFox Recovery for ${DEVICE} (${DEVICE_NAME}) // ${BUILD_DATE}" \
+        -F ./release-notes.md \
+        -R "https://github.com/${CIRRUS_REPO_FULL_NAME}"
+    
+    post_message
+else
+    echo -e "\033[0;36m##########################################\033[0m"
+    echo -e "\033[0;36m$(figlet "OrangeFox")\033[0m"
+    echo -e "\033[0;36m##########################################\033[0m"
+fi
---
+++ sync.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/env bash
+
+set -e
+export TERM=xterm-256color
+
+# Source config functions
+source .cirrus_config.sh
+
+timeStart
+echo -e "\033[0;36mSync OrangeFox Recovery...\033[0m"
+
+mkdir -p OrangeFox && cd OrangeFox
+git config --global user.name "${USER_NAME}"
+git config --global user.email "${USER_EMAIL}"
+
+# GitHub authentication if token provided
+if [[ -n "$GITHUB_TOKEN" ]]; then
+  echo "$GITHUB_TOKEN" > ikitoken.txt
+  gh auth login --with-token < ikitoken.txt
+fi
+
+git clone ${FOX_SYNC} && cd sync
+./orangefox_sync.sh --branch ${FOX_SYNC_BRANCH} --path ../fox_${FOX_SYNC_BRANCH}
+cd ../fox_${FOX_SYNC_BRANCH}
+
+# Add required components
+if [[ ! -d bootable/recovery/gui/theme ]]; then
+    echo -e "\033[0;36mAdding Theme...\033[0m"
+    git clone https://gitlab.com/OrangeFox/misc/theme.git bootable/recovery/gui/theme
+fi
+
+if [[ ! -d external/nano ]]; then
+    echo -e "\033[0;36mAdding Nano Library...\033[0m"
+    git clone https://github.com/LineageOS/android_external_nano -b lineage-19.1 external/nano
+fi
+
+if [[ ! -d external/libncurses ]]; then
+    echo -e "\033[0;36mAdding libncurses Library...\033[0m"
+    git clone https://github.com/LineageOS/android_external_libncurses -b lineage-19.1 external/libncurses
+fi
+
+if [[ ! -d external/bash ]]; then
+    echo -e "\033[0;36mAdding Bash Library...\033[0m"
+    git clone https://github.com/LineageOS/android_external_bash -b lineage-19.1 external/bash
+fi
+
+if [[ ! -d external/lptools ]]; then
+    echo -e "\033[0;36mAdding lptools Library...\033[0m"
+    git clone https://github.com/phhusson/vendor_lptools external/lptools
+fi
+
+if [[ -n "${MAINTAINER_URL}" ]]; then
+    echo -e "\033[0;36mChange Maintainer Profile...\033[0m"
+    wget ${MAINTAINER_URL} -O maintainer.png
+    cp -r maintainer.png bootable/recovery/gui/theme/portrait_hdpi/images/Default/About/
+fi
+
+if [[ -n "${KERNEL_TREE}" ]]; then
+    echo -e "\033[0;36mCloning Kernel Tree...\033[0m"
+    git clone ${KERNEL_TREE} -b ${KERNEL_BRANCH} ${KERNEL_PATH}
+fi
+
+if [[ -n "${COMMON_TREE}" ]]; then
+    echo -e "\033[0;36mCloning Common Tree...\033[0m"
+    git clone ${COMMON_TREE} -b ${COMMON_BRANCH} ${COMMON_PATH}
+fi
+
+if [[ -n "${DEVICE_TREE}" ]]; then
+    echo -e "\033[0;36mCloning Device Tree...\033[0m"
+    git clone ${DEVICE_TREE} -b ${DEVICE_TREE_BRANCH} ${DEVICE_PATH}
+fi
